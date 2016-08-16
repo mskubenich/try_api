f939428bb1a687db25d546e2f53c63122de95cb6
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2016-08-16 16:52:16 +0300

    Store static assets.

--- app/assets/javascripts/try_api/application.js.coffee
+++ app/assets/javascripts/try_api/application.js.coffee
@@ -1,10 +1,10 @@
-#= require bower_components/jquery/dist/jquery.js
-#= require bower_components/bootstrap/dist/js/bootstrap.min.js
-#= require bower_components/jquery-slimscroll/jquery.slimscroll.min.js
-#= require bower_components/highlightjs/highlight.pack.min.js
-#= require bower_components/angular/angular.js
-#= require bower_components/angular-bootstrap/ui-bootstrap-tpls.js
-#= require bower_components/angular-highlightjs/src/angular-highlightjs.js
+#= require try_api/bower_components/jquery/dist/jquery.js
+#= require try_api/bower_components/bootstrap/dist/js/bootstrap.min.js
+#= require try_api/bower_components/jquery-slimscroll/jquery.slimscroll.min.js
+#= require try_api/bower_components/highlightjs/highlight.pack.min.js
+#= require try_api/bower_components/angular/angular.js
+#= require try_api/bower_components/angular-bootstrap/ui-bootstrap-tpls.js
+#= require try_api/bower_components/angular-highlightjs/src/angular-highlightjs.js
 
 $ ->
   $('pre code').each (i, block) ->
@@ -12,4 +12,7 @@ $ ->
 
 
   $('.try-api-sidebar-menu').slimScroll
-    height: '100%'  
+    height: '100%'
+
+
+
--- lib/generators/try_api/install_generator.rb
+++ lib/generators/try_api/install_generator.rb
@@ -13,16 +13,16 @@ module TryApi
     def create_config_file
       template 'config/try_api.yml', 'config/try_api.yml'
 
-      Bundler.with_clean_env do
-        run "mkdir vendor/assets/try_api"
-        run "cd vendor/assets/try_api && bower install jquery"
-        run "cd vendor/assets/try_api && bower install bootstrap"
-        run "cd vendor/assets/try_api && bower install slimScroll"
-        run "cd vendor/assets/try_api && bower install highlightjs"
-        run "cd vendor/assets/try_api && bower install angular"
-        run "cd vendor/assets/try_api && bower install angular-bootstrap"
-        run "cd vendor/assets/try_api && bower install angular-highlightjs"
-      end
+      # Bundler.with_clean_env do
+      #   run "mkdir vendor/assets/try_api"
+      #   run "cd vendor/assets/try_api && bower install jquery"
+      #   run "cd vendor/assets/try_api && bower install bootstrap"
+      #   run "cd vendor/assets/try_api && bower install slimScroll"
+      #   run "cd vendor/assets/try_api && bower install highlightjs"
+      #   run "cd vendor/assets/try_api && bower install angular"
+      #   run "cd vendor/assets/try_api && bower install angular-bootstrap"
+      #   run "cd vendor/assets/try_api && bower install angular-highlightjs"
+      # end
 
     end
   end
