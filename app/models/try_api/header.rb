9e42298d55f64d35a037a3f6e3eb20ab767ed18c
Mykhaylo Skubenych <mykhaylo.skubenych@gmail.com>
2016-08-29 10:34:59 +0300

    Fix typesafe accessor usage.

--- app/models/try_api/header.rb
+++ app/models/try_api/header.rb
@@ -2,7 +2,7 @@ module TryApi
   class Header < TryApi::Base
     typesafe_accessor :name, String
     typesafe_accessor :description, String
-    typesafe_accessor :global, Boolean, false
+    typesafe_accessor :global, Boolean, default: false
 
     class << self
       def parse(hash)
